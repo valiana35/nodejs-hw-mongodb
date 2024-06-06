a3bf323561f86d1679adb35d4f5bfd7b86b072f9
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-06-06 11:45:15 +0300

    nodehw3

--- src/server.js
+++ src/server.js
@@ -11,7 +11,10 @@ const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(express.json({
+    type: ['application/json', 'application/vnd.api+json'],
+  }));
+  
   app.use(cors());
 
   app.use(
