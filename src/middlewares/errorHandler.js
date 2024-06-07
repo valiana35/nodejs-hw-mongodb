0dcbcf0f1aeb87176b6d4c06b2bfdfa70affd9d2
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-06-07 11:19:44 +0300

    nodehw3

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 import { HttpError } from 'http-errors';
 
-export const errorHandler = (err, req, res) => {
+export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
@@ -14,4 +14,5 @@ export const errorHandler = (err, req, res) => {
     message: 'Something went wrong',
     data: err.message,
   });
+  next();
 };
--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,6 +1,7 @@
-export const notFoundHandler = (req, res) => {
+export const notFoundHandler = (req, res, next) => {
     res.status(404).json({
       status: 404,
       message: 'Route not found',
     });
+    next();
   };
